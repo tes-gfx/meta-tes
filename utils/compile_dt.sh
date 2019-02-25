b17529839c79080a2fd34e570108bfb4518cc84f
Markus Hertel <markus.hertel@tes-dst.com>
2019-02-25 11:33:17 +0100

    use symbols in dtc

--- utils/compile_dt.sh
+++ utils/compile_dt.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 KERNEL_SRC=/home/fpga/yocto/yocto/build/tmp/work-shared/arria10/kernel-source
+DTC=/opt/poky/2.4.3/sysroots/x86_64-pokysdk-linux/usr/bin/dtc
 INPUT=$1
 INTERMED=$(mktemp)
 OUTPUT=${INPUT%.dts}.dtb
 
 cpp -nostdinc -I ${KERNEL_SRC}/arch/arm/boot/dts -I ${KERNEL_SRC}/arch/arm/boot/dts/include -undef -x assembler-with-cpp $INPUT > $INTERMED
-dtc -I dts -O dtb -o $OUTPUT $INTERMED
+$DTC -I dts -O dtb -@ -o $OUTPUT $INTERMED
 
 rm $INTERMED
